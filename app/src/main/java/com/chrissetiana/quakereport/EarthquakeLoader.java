1a02146f2c262ff334710e62fe2210241a747f70
chrissetiana <chrissetiana@gmail.com>
2018-06-15 11:47:43 +0100

    Modify Loader class

--- app/src/main/java/com/chrissetiana/quakereport/EarthquakeLoader.java
+++ app/src/main/java/com/chrissetiana/quakereport/EarthquakeLoader.java
@@ -1,9 +1,33 @@
 package com.chrissetiana.quakereport;
 
 import android.content.AsyncTaskLoader;
+import android.content.Context;
 
 import java.util.List;
 
 public class EarthquakeLoader extends AsyncTaskLoader<List<Earthquake>> {
+
+    private static final String LOG_TAG = EarthquakeLoader.class.getName();
+    private String url;
+
+    public EarthquakeLoader(Context context, String url) {
+        super(context);
+        this.url = url;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+    }
+
+    @Override
+    public List<Earthquake> loadInBackground() {
+        if (url == null) {
+            return null;
+        }
+
+        List<Earthquake> earthquakes = QueryUtils.fetchEarthquakeData(url);
+        return earthquakes;
+    }
 
 }
