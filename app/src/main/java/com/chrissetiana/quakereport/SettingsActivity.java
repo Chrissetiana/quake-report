45f868e8a9654a4f3d231679b629de21a31688c2
chrissetiana <chrissetiana@gmail.com>
2018-06-20 23:25:07 +0100

    Add preference - minimum data

--- app/src/main/java/com/chrissetiana/quakereport/SettingsActivity.java
+++ app/src/main/java/com/chrissetiana/quakereport/SettingsActivity.java
@@ -8,10 +8,15 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        setContentView(R.layout.settings_activity);
     }
 
     public static class EarthquakePreferenceFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings_main);
+        }
     }
 
 }
