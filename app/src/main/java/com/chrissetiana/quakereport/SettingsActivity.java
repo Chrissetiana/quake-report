04d030e9e38c1dcf37ccb2afb015e0adb45e879d
chrissetiana <chrissetiana@gmail.com>
2018-06-20 23:39:46 +0100

    Add preference - summary

--- app/src/main/java/com/chrissetiana/quakereport/SettingsActivity.java
+++ app/src/main/java/com/chrissetiana/quakereport/SettingsActivity.java
@@ -1,7 +1,10 @@
 package com.chrissetiana.quakereport;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 
 public class SettingsActivity extends AppCompatActivity {
@@ -11,11 +14,28 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.settings_activity);
     }
 
-    public static class EarthquakePreferenceFragment extends PreferenceFragment {
+    public static class EarthquakePreferenceFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.settings_main);
+
+            Preference minMag = findPreference(getString(R.string.settings_min_magnitude_key));
+            bindPreferenceSummaryToValue(minMag);
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object value) {
+            String strVal = value.toString();
+            preference.setSummary(strVal);
+            return true;
+        }
+
+        private void bindPreferenceSummaryToValue(Preference preference) {
+            preference.setOnPreferenceChangeListener(this);
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+            String preferenceString = preferences.getString(preference.getKey(), "");
+            onPreferenceChange(preference, preferenceString);
         }
     }
 
