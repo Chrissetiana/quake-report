8d65324617142243630086d5c851895131a12159
chrissetiana <chrissetiana@gmail.com>
2018-06-20 22:38:57 +0100

    Modify settings activity

--- app/src/main/java/com/chrissetiana/quakereport/SettingsActivity.java
+++ app/src/main/java/com/chrissetiana/quakereport/SettingsActivity.java
@@ -1,4 +1,17 @@
 package com.chrissetiana.quakereport;
 
-public class SettingsActivity {
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+
+    public static class EarthquakePreferenceFragment extends PreferenceFragment {
+    }
+
 }
